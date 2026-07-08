f2b1c6ab367a3a9f9c822a753de636672d410224
devpolas <195308698+devpolas@users.noreply.github.com>
2026-07-08 22:40:31 +0600

    fix property image field

---
+++ prisma/migrations/20260708123312_fix_property_image_fields/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `publicId` on the `property_images` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "property_images" DROP COLUMN "publicId";
