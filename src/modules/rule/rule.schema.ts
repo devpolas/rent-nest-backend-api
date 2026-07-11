9fc070c856977d5f86f7caaaa98d55ad9bc2114c
devpolas <195308698+devpolas@users.noreply.github.com>
2026-07-11 16:25:13 +0600

    fix rule name and slug length

--- src/modules/rule/rule.schema.ts
+++ src/modules/rule/rule.schema.ts
@@ -1,12 +1,12 @@
 import * as z from "zod";
 
 export const RuleTypeSchema = z.object({
-  name: z.string().min(1).max(100),
+  name: z.string().min(1).max(255),
   icon: z.string().optional(),
 });
 
 export const RuleUpdateTypeSchema = z.object({
-  name: z.string().min(1).max(100).optional(),
+  name: z.string().min(1).max(255).optional(),
   icon: z.string().optional(),
 });
 
