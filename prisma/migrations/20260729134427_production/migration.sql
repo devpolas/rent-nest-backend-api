abd1a2d0e73883b288057e129dfdec268fb06378
Polas Chandra Barmon <195308698+devpolas@users.noreply.github.com>
2026-07-29 19:55:40 +0600

    chor: make all schema for production

---
+++ prisma/migrations/20260729134427_production/migration.sql
@@ -0,0 +1,550 @@
+-- CreateEnum
+CREATE TYPE "UserStatus" AS ENUM ('ACTIVE', 'DEACTIVATE', 'BLOCKED', 'BANNED');
+
+-- CreateEnum
+CREATE TYPE "UserRole" AS ENUM ('TENANT', 'LANDLORD', 'MODERATOR', 'ADMIN');
+
+-- CreateEnum
+CREATE TYPE "SocialPlatform" AS ENUM ('GITHUB', 'LINKEDIN', 'FACEBOOK', 'TWITTER', 'INSTAGRAM', 'YOUTUBE', 'DISCORD', 'TELEGRAM', 'WHATSAPP', 'WEBSITE');
+
+-- CreateEnum
+CREATE TYPE "LocationType" AS ENUM ('HOME', 'CURRENT', 'WORK', 'PROPERTY');
+
+-- CreateEnum
+CREATE TYPE "PropertyStatus" AS ENUM ('PENDING', 'APPROVED', 'REJECTED', 'RENTED', 'ARCHIVED');
+
+-- CreateEnum
+CREATE TYPE "AvailabilityStatus" AS ENUM ('AVAILABLE', 'RESERVED', 'RENTED', 'UNAVAILABLE');
+
+-- CreateEnum
+CREATE TYPE "RentalRequestStatus" AS ENUM ('PENDING', 'APPROVED', 'REJECTED', 'PAYMENT_PENDING', 'ACTIVE', 'COMPLETED', 'CANCELLED', 'EXPIRED');
+
+-- CreateEnum
+CREATE TYPE "PaymentStatus" AS ENUM ('PENDING', 'SUCCESS', 'FAILED', 'CANCELLED', 'EXPIRED', 'REFUNDED');
+
+-- CreateTable
+CREATE TABLE "account_session" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "refreshTokenHash" TEXT NOT NULL,
+    "browser" TEXT,
+    "operatingSystem" TEXT,
+    "deviceType" TEXT,
+    "ipAddress" TEXT,
+    "userAgent" TEXT,
+    "isRevoked" BOOLEAN NOT NULL DEFAULT false,
+    "revokedAt" TIMESTAMP(3),
+    "expiresAt" TIMESTAMP(3) NOT NULL,
+    "lastUsedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "account_session_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "amenities" (
+    "id" TEXT NOT NULL,
+    "amenityNo" SERIAL NOT NULL,
+    "name" VARCHAR(100) NOT NULL,
+    "slug" VARCHAR(120) NOT NULL,
+    "icon" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "amenities_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "features" (
+    "id" TEXT NOT NULL,
+    "featureNo" SERIAL NOT NULL,
+    "name" VARCHAR(100) NOT NULL,
+    "slug" VARCHAR(120) NOT NULL,
+    "icon" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "features_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "locations" (
+    "id" TEXT NOT NULL,
+    "latitude" TEXT,
+    "longitude" TEXT,
+    "type" "LocationType" NOT NULL,
+    "country" TEXT NOT NULL,
+    "division" TEXT NOT NULL,
+    "district" TEXT NOT NULL,
+    "city" TEXT NOT NULL,
+    "village" TEXT NOT NULL,
+    "postalCode" TEXT NOT NULL,
+    "addressLine" TEXT,
+    "profileId" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "locations_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "payments" (
+    "id" TEXT NOT NULL,
+    "amount" DECIMAL(10,2) NOT NULL,
+    "currency" VARCHAR(20) NOT NULL,
+    "provider" VARCHAR(20) NOT NULL,
+    "transactionId" TEXT NOT NULL,
+    "expireIn" TIMESTAMP(3) NOT NULL,
+    "status" "PaymentStatus" NOT NULL DEFAULT 'PENDING',
+    "propertyId" TEXT NOT NULL,
+    "tenantId" TEXT NOT NULL,
+    "landlordId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "payments_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "profiles" (
+    "id" TEXT NOT NULL,
+    "profileImage" TEXT,
+    "bio" TEXT,
+    "birthdate" TIMESTAMP(3),
+    "userId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "profiles_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "properties" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "slug" TEXT NOT NULL,
+    "rent" DECIMAL(10,2) NOT NULL,
+    "securityDeposit" DECIMAL(10,2) NOT NULL,
+    "bedrooms" INTEGER NOT NULL,
+    "bathrooms" INTEGER NOT NULL,
+    "area" DECIMAL(10,2) NOT NULL,
+    "availableFrom" TIMESTAMP(3),
+    "availability" "AvailabilityStatus" NOT NULL DEFAULT 'AVAILABLE',
+    "status" "PropertyStatus" NOT NULL DEFAULT 'PENDING',
+    "locationId" TEXT,
+    "landlordId" TEXT NOT NULL,
+    "categoryId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "properties_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "property_amenities" (
+    "id" TEXT NOT NULL,
+    "propertyId" TEXT NOT NULL,
+    "amenityId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "property_amenities_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "property_categories" (
+    "id" TEXT NOT NULL,
+    "categoryNo" SERIAL NOT NULL,
+    "name" VARCHAR(100) NOT NULL,
+    "slug" VARCHAR(120) NOT NULL,
+    "icon" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "property_categories_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "property_features" (
+    "id" TEXT NOT NULL,
+    "propertyId" TEXT NOT NULL,
+    "featureId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "property_features_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "property_images" (
+    "id" TEXT NOT NULL,
+    "propertyId" TEXT NOT NULL,
+    "url" TEXT NOT NULL,
+    "isThumbnail" BOOLEAN NOT NULL DEFAULT false,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "property_images_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "property_rules" (
+    "id" TEXT NOT NULL,
+    "propertyId" TEXT NOT NULL,
+    "ruleId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "property_rules_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "rental_requests" (
+    "id" TEXT NOT NULL,
+    "tenantId" TEXT NOT NULL,
+    "propertyId" TEXT NOT NULL,
+    "landlordId" TEXT NOT NULL,
+    "message" TEXT NOT NULL,
+    "moveInDate" TIMESTAMP(3) NOT NULL,
+    "leaseDays" INTEGER NOT NULL,
+    "status" "RentalRequestStatus" NOT NULL DEFAULT 'PENDING',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "rental_requests_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "reviews" (
+    "id" TEXT NOT NULL,
+    "tenantId" TEXT NOT NULL,
+    "propertyId" TEXT NOT NULL,
+    "rating" INTEGER NOT NULL,
+    "comment" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "reviews_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "rules" (
+    "id" TEXT NOT NULL,
+    "ruleNo" SERIAL NOT NULL,
+    "name" VARCHAR(255) NOT NULL,
+    "slug" VARCHAR(300) NOT NULL,
+    "icon" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "rules_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "social_profile" (
+    "id" TEXT NOT NULL,
+    "platform" "SocialPlatform" NOT NULL,
+    "url" TEXT NOT NULL,
+    "profileId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "social_profile_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL,
+    "name" VARCHAR(255) NOT NULL,
+    "email" VARCHAR(255) NOT NULL,
+    "password" TEXT NOT NULL,
+    "phone" VARCHAR(20),
+    "avatar" TEXT,
+    "emailVerified" BOOLEAN NOT NULL DEFAULT false,
+    "passwordResetToken" TEXT,
+    "passwordResetExpires" TIMESTAMP(3),
+    "role" "UserRole" NOT NULL DEFAULT 'TENANT',
+    "status" "UserStatus" NOT NULL DEFAULT 'ACTIVE',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "account_session_refreshTokenHash_key" ON "account_session"("refreshTokenHash");
+
+-- CreateIndex
+CREATE INDEX "account_session_userId_idx" ON "account_session"("userId");
+
+-- CreateIndex
+CREATE INDEX "account_session_expiresAt_idx" ON "account_session"("expiresAt");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "amenities_name_key" ON "amenities"("name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "amenities_slug_key" ON "amenities"("slug");
+
+-- CreateIndex
+CREATE INDEX "amenities_amenityNo_idx" ON "amenities"("amenityNo");
+
+-- CreateIndex
+CREATE INDEX "amenities_slug_idx" ON "amenities"("slug");
+
+-- CreateIndex
+CREATE INDEX "amenities_name_idx" ON "amenities"("name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "features_name_key" ON "features"("name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "features_slug_key" ON "features"("slug");
+
+-- CreateIndex
+CREATE INDEX "features_featureNo_idx" ON "features"("featureNo");
+
+-- CreateIndex
+CREATE INDEX "features_slug_idx" ON "features"("slug");
+
+-- CreateIndex
+CREATE INDEX "features_name_idx" ON "features"("name");
+
+-- CreateIndex
+CREATE INDEX "locations_type_idx" ON "locations"("type");
+
+-- CreateIndex
+CREATE INDEX "locations_country_idx" ON "locations"("country");
+
+-- CreateIndex
+CREATE INDEX "locations_division_idx" ON "locations"("division");
+
+-- CreateIndex
+CREATE INDEX "locations_district_idx" ON "locations"("district");
+
+-- CreateIndex
+CREATE INDEX "locations_city_idx" ON "locations"("city");
+
+-- CreateIndex
+CREATE INDEX "locations_postalCode_idx" ON "locations"("postalCode");
+
+-- CreateIndex
+CREATE INDEX "locations_country_division_district_city_idx" ON "locations"("country", "division", "district", "city");
+
+-- CreateIndex
+CREATE INDEX "locations_profileId_idx" ON "locations"("profileId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "payments_transactionId_key" ON "payments"("transactionId");
+
+-- CreateIndex
+CREATE INDEX "payments_expireIn_idx" ON "payments"("expireIn");
+
+-- CreateIndex
+CREATE INDEX "payments_tenantId_idx" ON "payments"("tenantId");
+
+-- CreateIndex
+CREATE INDEX "payments_landlordId_idx" ON "payments"("landlordId");
+
+-- CreateIndex
+CREATE INDEX "payments_propertyId_idx" ON "payments"("propertyId");
+
+-- CreateIndex
+CREATE INDEX "payments_amount_idx" ON "payments"("amount");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "payments_propertyId_tenantId_status_key" ON "payments"("propertyId", "tenantId", "status");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "profiles_userId_key" ON "profiles"("userId");
+
+-- CreateIndex
+CREATE INDEX "profiles_userId_idx" ON "profiles"("userId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "properties_slug_key" ON "properties"("slug");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "properties_locationId_key" ON "properties"("locationId");
+
+-- CreateIndex
+CREATE INDEX "properties_landlordId_idx" ON "properties"("landlordId");
+
+-- CreateIndex
+CREATE INDEX "properties_categoryId_idx" ON "properties"("categoryId");
+
+-- CreateIndex
+CREATE INDEX "properties_status_idx" ON "properties"("status");
+
+-- CreateIndex
+CREATE INDEX "properties_availability_idx" ON "properties"("availability");
+
+-- CreateIndex
+CREATE INDEX "properties_rent_idx" ON "properties"("rent");
+
+-- CreateIndex
+CREATE INDEX "properties_createdAt_idx" ON "properties"("createdAt");
+
+-- CreateIndex
+CREATE INDEX "property_amenities_propertyId_idx" ON "property_amenities"("propertyId");
+
+-- CreateIndex
+CREATE INDEX "property_amenities_amenityId_idx" ON "property_amenities"("amenityId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "property_amenities_propertyId_amenityId_key" ON "property_amenities"("propertyId", "amenityId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "property_categories_name_key" ON "property_categories"("name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "property_categories_slug_key" ON "property_categories"("slug");
+
+-- CreateIndex
+CREATE INDEX "property_categories_categoryNo_idx" ON "property_categories"("categoryNo");
+
+-- CreateIndex
+CREATE INDEX "property_categories_name_idx" ON "property_categories"("name");
+
+-- CreateIndex
+CREATE INDEX "property_categories_slug_idx" ON "property_categories"("slug");
+
+-- CreateIndex
+CREATE INDEX "property_features_propertyId_idx" ON "property_features"("propertyId");
+
+-- CreateIndex
+CREATE INDEX "property_features_featureId_idx" ON "property_features"("featureId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "property_features_propertyId_featureId_key" ON "property_features"("propertyId", "featureId");
+
+-- CreateIndex
+CREATE INDEX "property_images_propertyId_idx" ON "property_images"("propertyId");
+
+-- CreateIndex
+CREATE INDEX "property_rules_propertyId_idx" ON "property_rules"("propertyId");
+
+-- CreateIndex
+CREATE INDEX "property_rules_ruleId_idx" ON "property_rules"("ruleId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "property_rules_propertyId_ruleId_key" ON "property_rules"("propertyId", "ruleId");
+
+-- CreateIndex
+CREATE INDEX "rental_requests_propertyId_tenantId_idx" ON "rental_requests"("propertyId", "tenantId");
+
+-- CreateIndex
+CREATE INDEX "rental_requests_tenantId_idx" ON "rental_requests"("tenantId");
+
+-- CreateIndex
+CREATE INDEX "rental_requests_propertyId_idx" ON "rental_requests"("propertyId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "rental_requests_tenantId_propertyId_key" ON "rental_requests"("tenantId", "propertyId");
+
+-- CreateIndex
+CREATE INDEX "reviews_tenantId_propertyId_idx" ON "reviews"("tenantId", "propertyId");
+
+-- CreateIndex
+CREATE INDEX "reviews_propertyId_idx" ON "reviews"("propertyId");
+
+-- CreateIndex
+CREATE INDEX "reviews_tenantId_idx" ON "reviews"("tenantId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "rules_name_key" ON "rules"("name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "rules_slug_key" ON "rules"("slug");
+
+-- CreateIndex
+CREATE INDEX "rules_ruleNo_idx" ON "rules"("ruleNo");
+
+-- CreateIndex
+CREATE INDEX "rules_slug_idx" ON "rules"("slug");
+
+-- CreateIndex
+CREATE INDEX "rules_name_idx" ON "rules"("name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "social_profile_platform_profileId_key" ON "social_profile"("platform", "profileId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_passwordResetToken_key" ON "users"("passwordResetToken");
+
+-- CreateIndex
+CREATE INDEX "users_name_idx" ON "users"("name");
+
+-- CreateIndex
+CREATE INDEX "users_email_idx" ON "users"("email");
+
+-- CreateIndex
+CREATE INDEX "users_phone_idx" ON "users"("phone");
+
+-- AddForeignKey
+ALTER TABLE "account_session" ADD CONSTRAINT "account_session_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "locations" ADD CONSTRAINT "locations_profileId_fkey" FOREIGN KEY ("profileId") REFERENCES "profiles"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "payments" ADD CONSTRAINT "payments_tenantId_fkey" FOREIGN KEY ("tenantId") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "payments" ADD CONSTRAINT "payments_landlordId_fkey" FOREIGN KEY ("landlordId") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "profiles" ADD CONSTRAINT "profiles_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "properties" ADD CONSTRAINT "properties_locationId_fkey" FOREIGN KEY ("locationId") REFERENCES "locations"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "properties" ADD CONSTRAINT "properties_landlordId_fkey" FOREIGN KEY ("landlordId") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "properties" ADD CONSTRAINT "properties_categoryId_fkey" FOREIGN KEY ("categoryId") REFERENCES "property_categories"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "property_amenities" ADD CONSTRAINT "property_amenities_propertyId_fkey" FOREIGN KEY ("propertyId") REFERENCES "properties"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "property_amenities" ADD CONSTRAINT "property_amenities_amenityId_fkey" FOREIGN KEY ("amenityId") REFERENCES "amenities"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "property_features" ADD CONSTRAINT "property_features_propertyId_fkey" FOREIGN KEY ("propertyId") REFERENCES "properties"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "property_features" ADD CONSTRAINT "property_features_featureId_fkey" FOREIGN KEY ("featureId") REFERENCES "features"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "property_images" ADD CONSTRAINT "property_images_propertyId_fkey" FOREIGN KEY ("propertyId") REFERENCES "properties"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "property_rules" ADD CONSTRAINT "property_rules_propertyId_fkey" FOREIGN KEY ("propertyId") REFERENCES "properties"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "property_rules" ADD CONSTRAINT "property_rules_ruleId_fkey" FOREIGN KEY ("ruleId") REFERENCES "rules"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "rental_requests" ADD CONSTRAINT "rental_requests_propertyId_fkey" FOREIGN KEY ("propertyId") REFERENCES "properties"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "rental_requests" ADD CONSTRAINT "rental_requests_tenantId_fkey" FOREIGN KEY ("tenantId") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "rental_requests" ADD CONSTRAINT "rental_requests_landlordId_fkey" FOREIGN KEY ("landlordId") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "reviews" ADD CONSTRAINT "reviews_tenantId_fkey" FOREIGN KEY ("tenantId") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "reviews" ADD CONSTRAINT "reviews_propertyId_fkey" FOREIGN KEY ("propertyId") REFERENCES "properties"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "social_profile" ADD CONSTRAINT "social_profile_profileId_fkey" FOREIGN KEY ("profileId") REFERENCES "profiles"("id") ON DELETE CASCADE ON UPDATE CASCADE;
