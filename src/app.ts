7c50a405a1768b55dbe2b80e3ac319c58f7c3cc1
Polas Chandra Barmon <195308698+devpolas@users.noreply.github.com>
2026-07-30 15:54:07 +0600

    middleware: add express-session middleware

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import session from "express-session";
 import type { Application, Request, Response } from "express";
 import cors from "cors";
 import { authRouter } from "./modules/auth/auth.routes";
@@ -19,27 +20,53 @@ import { profileRouter } from "./modules/profile/profile.route";
 import { locationRouter } from "./modules/location/location.route";
 import { socialProfileRouter } from "./modules/social-profile/social-profile.route";
 import { propertyImageRouter } from "./modules/property-images/property-image.route";
+import { sendResponse } from "./utils/sendResponse";
+import httpStatus from "http-status";
 
 const app: Application = express();
 
 app.set("trust proxy", true);
+app.use(
+  session({
+    secret: config.session_secret,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      secure: config.node_env === "production",
+      httpOnly: true,
+      maxAge: 1000 * 60 * 10,
+    },
+  }),
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({ origin: config.app_urls, credentials: true }));
 app.use(cookieParser());
 
-app.get("/", (req: Request, res: Response) => {
-  res.status(200).json({
+app.get("/", (_req: Request, res: Response) => {
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
     success: true,
-    message: "Rent Nest Ready To Talk",
+    message: "Rent Nest is ready to talk!",
   });
 });
 
-app.get("/health", (req: Request, res: Response) => {
-  res.status(200).json({
+app.get("/health", (_req: Request, res: Response) => {
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
     success: true,
-    status: "OK",
-    timestamp: new Date().toISOString(),
+    message: "Rent Nest is healthy!",
+  });
+});
+
+app.get("/version", (_req: Request, res: Response) => {
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: "Version information retrieved successfully.",
+    data: {
+      version: "0.1.0",
+    },
   });
 });
 
