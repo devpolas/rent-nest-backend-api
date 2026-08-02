236412ce8176ebbafa0192fd3c77e9efbec102d3
Polas Chandra Barmon <195308698+devpolas@users.noreply.github.com>
2026-08-02 18:22:49 +0600

    improve: improve image controller for reuseable folder

--- src/modules/image/image.controller.ts
+++ src/modules/image/image.controller.ts
@@ -1,20 +1,22 @@
-import type { NextFunction, Request, Response } from "express";
-
-import { catchAsync } from "../../utils/catchAsync";
-import { uploadImage } from "./image.service";
-import { sendResponse } from "../../utils/sendResponse";
+import type { Request, Response } from "express";
 import httpStatus from "http-status";
 
+import { catchAsync } from "../../utils/catchAsync";
+import { sendResponse } from "../../utils/sendResponse";
+import { uploadImage } from "./image.service";
+
 export const uploadImagesController = catchAsync(
-  async (req: Request, res: Response, next: NextFunction) => {
+  async (req: Request, res: Response) => {
     const files = req.files as Express.Multer.File[];
+
+    const folder = (req.query.folder as string) || "rent_nest";
 
     if (!files || files.length === 0) {
       throw new Error("No images uploaded");
     }
 
     const uploaded = await Promise.all(
-      files.map((file) => uploadImage(file, "rent-nest/properties")),
+      files.map((file) => uploadImage(file, folder)),
     );
 
     const images = uploaded.map((image) => ({
@@ -26,6 +28,7 @@ export const uploadImagesController = catchAsync(
       success: true,
       statusCode: httpStatus.OK,
       message: "Images uploaded successfully",
+
       data: {
         images,
       },
