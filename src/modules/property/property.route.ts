bd211e6882ecec855d7d85b48ddbf1271955de6e
Polas Chandra Barmon <195308698+devpolas@users.noreply.github.com>
2026-08-10 13:36:56 +0600

    fix: routing logic

--- src/modules/property/property.route.ts
+++ src/modules/property/property.route.ts
@@ -14,24 +14,20 @@ import { propertyImageRouter } from "../property-images/property-image.route";
 
 const router = Router();
 
-
 router.use("/:propertyId/images", propertyImageRouter);
 router.use("/:propertyId/reviews", reviewRouter);
 
-
 router.get("/", getAllProperties);
 
-
-router.use(protect);
-
 // Must come before /:id
-router.get("/my-properties", restrictTo("LANDLORD"), getAllProperties);
+router.get("/my-properties", protect, restrictTo("LANDLORD"), getAllProperties);
 
-router.post("/", restrictTo("LANDLORD"), createProperty);
+router.post("/", protect, restrictTo("LANDLORD"), createProperty);
 
 // Public property details
 router.get("/:id", getPropertyById);
 
+router.use(protect);
 // Protected property management
 router
   .route("/:id")
