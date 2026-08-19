3c9c0dbd249f03be5792a9a3a4659b762fb718b9
Polas Chandra Barmon <195308698+devpolas@users.noreply.github.com>
2026-08-20 00:39:07 +0600

    fix: remove auth when upload image

--- src/modules/image/image.routes.ts
+++ src/modules/image/image.routes.ts
@@ -1,13 +1,10 @@
 import { Router } from "express";
 
 import upload from "../../middlewares/upload";
-import { protect } from "../../middlewares/auth";
 
 import { uploadImagesController } from "./image.controller";
 
 const router = Router();
-
-router.use(protect);
 
 router.post("/upload", upload.array("images", 10), uploadImagesController);
 
